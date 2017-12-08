a60f2f7d9c2cf01053b104ba50477ffd66eeacaa
s-waldron <30762163+s-waldron@users.noreply.github.com>
2017-12-08 17:19:40 -0500

    Added Bootstrap and a navigation bar to the site.

--- ElderSourceVolunteerManagementCore/Startup.cs
+++ ElderSourceVolunteerManagementCore/Startup.cs
@@ -22,6 +22,7 @@ namespace ElderSourceVolunteerManagementCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
     }
