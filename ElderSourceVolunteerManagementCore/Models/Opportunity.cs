5e46cd04f18398706fdef0ff00bc6b170b984e48
s-waldron <30762163+s-waldron@users.noreply.github.com>
2017-12-18 12:27:28 -0500

    Fixing alignment issues.

--- ElderSourceVolunteerManagementCore/Models/Opportunity.cs
+++ ElderSourceVolunteerManagementCore/Models/Opportunity.cs
@@ -12,7 +12,18 @@ namespace ElderSourceVolunteerManagementCore.Models
         public DateTime OpportunityDate { get; set; }
         public Boolean OpportunityElderSource { get; set; }
         public Boolean OpportunityElderSourceInstitute { get; set; }
-        public string OpportunityInterest { get; set; }
+        public Boolean OpportunityNutrition { get; set; }
+        public Boolean OpportunityTrainingFacilitator { get; set; }
+        public Boolean OpportunityAdmin { get; set; }
+        public Boolean OpportunityGeneral { get; set; }
+        public Boolean OpportunityHealthyLiving { get; set; }
+        public Boolean OpportunityDiseaseManagement { get; set; }
+        public Boolean OpportunityFallsPrevention { get; set; }
+        public Boolean OpportunityCaregiver { get; set; }
+        public Boolean OpportunityMentalWellness { get; set; }
+        public Boolean OpportunityEvents { get; set; }
+        public Boolean OpportunitySocialMedia { get; set; }
+        public string OpportunityOtherInterest { get; set; }
         public string OpportunityDescription { get; set; }
     }// end Opportunity class
 }
--- ElderSourceVolunteerManagementCore/Startup.cs
+++ ElderSourceVolunteerManagementCore/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace ElderSourceVolunteerManagementCore
 {
