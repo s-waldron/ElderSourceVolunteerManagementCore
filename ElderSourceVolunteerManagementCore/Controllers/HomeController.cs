7c2989ddf9db3d35b6c917fa6599339a69c4b519
s-waldron <30762163+s-waldron@users.noreply.github.com>
2018-01-04 22:58:04 -0500

    Working on the main homepage for the application.  I have the opportunities displayed out of the database and I have a dropdown menu that shows the interest categories to the user.  Once an interest is selected that is the filter for the output from the database back on the screen.  Still need to have a shopping cart so that the volunteer can select opportunities to work on and then fill out the volunteer form then this section is complete.

--- ElderSourceVolunteerManagementCore/Controllers/HomeController.cs
+++ ElderSourceVolunteerManagementCore/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ElderSourceVolunteerManagementCore.Models;
+using System.Linq;
+using ElderSourceVolunteerManagementCore.Models.ViewModels;
 
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -16,6 +18,86 @@ namespace ElderSourceVolunteerManagementCore.Controllers
         }
 
         // GET: /<controller>/
-        public IActionResult Index() => View(repository.Opportunity);
+        public ViewResult Index(string Interest)
+        {
+            switch (Interest)
+            {
+                case "nt":
+                    return View(new InterestViewModel {
+                        Opportunity = repository.Opportunity
+                        .Where(o => Interest == null || o.OpportunityNutrition == true)
+                    });
+                case "tf":
+                    return View(new InterestViewModel {
+                        Opportunity = repository.Opportunity
+                        .Where(o => Interest == null || o.OpportunityTrainingFacilitator == true)
+                    });
+                case "am":
+                    return View(new InterestViewModel
+                    {
+                        Opportunity = repository.Opportunity
+                        .Where(o => Interest == null || o.OpportunityAdmin == true)
+                    });
+                case "gn":
+                    return View(new InterestViewModel
+                    {
+                        Opportunity = repository.Opportunity
+                        .Where(o => Interest == null || o.OpportunityGeneral == true)
+                    });
+                case "hl":
+                    return View(new InterestViewModel
+                    {
+                        Opportunity = repository.Opportunity
+                        .Where(o => Interest == null || o.OpportunityHealthyLiving == true)
+                    });
+                case "dm":
+                    return View(new InterestViewModel
+                    {
+                        Opportunity = repository.Opportunity
+                        .Where(o => Interest == null || o.OpportunityDiseaseManagement == true)
+                    });
+                case "fp":
+                    return View(new InterestViewModel
+                    {
+                        Opportunity = repository.Opportunity
+                        .Where(o => Interest == null || o.OpportunityFallsPrevention == true)
+                    });
+                case "cg":
+                    return View(new InterestViewModel
+                    {
+                        Opportunity = repository.Opportunity
+                        .Where(o => Interest == null || o.OpportunityCaregiver == true)
+                    });
+                case "mw":
+                    return View(new InterestViewModel
+                    {
+                        Opportunity = repository.Opportunity
+                        .Where(o => Interest == null || o.OpportunityMentalWellness == true)
+                    });
+                case "ev":
+                    return View(new InterestViewModel
+                    {
+                        Opportunity = repository.Opportunity
+                        .Where(o => Interest == null || o.OpportunityEvents == true)
+                    });
+                case "sm":
+                    return View(new InterestViewModel
+                    {
+                        Opportunity = repository.Opportunity
+                        .Where(o => Interest == null || o.OpportunitySocialMedia == true)
+                    });
+                case "oi":
+                    return View(new InterestViewModel
+                    {
+                        Opportunity = repository.Opportunity
+                        .Where(o => Interest == null || o.OpportunityOtherInterest != null || o.OpportunityOtherInterest != "")
+                    });
+                default:
+                    return View(new InterestViewModel {
+                        Opportunity = repository.Opportunity
+                    });
+            }
+        }
+        
     }
 }
---
+++ ElderSourceVolunteerManagementCore/Models/ViewModels/InterestViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ElderSourceVolunteerManagementCore.Models.ViewModels
+{
+    public class InterestViewModel
+    {
+
+        public IEnumerable<Opportunity> Opportunity { get; set; }
+
+        public string Interest { get; set; }
+
+        public List<SelectListItem> Interests { get; } = new List<SelectListItem>
+        {
+            new SelectListItem {Value="nt", Text="Nutrition"},
+            new SelectListItem {Value="tf", Text="Training Facilitator"},
+            new SelectListItem {Value="am", Text="Admin"},
+            new SelectListItem {Value="gn", Text="General"},
+            new SelectListItem {Value="hl", Text="Healthy Living"},
+            new SelectListItem {Value="dm", Text="Disease Management"},
+            new SelectListItem {Value="fp", Text="Falls Prevention"},
+            new SelectListItem {Value="cg", Text="Caregiver"},
+            new SelectListItem {Value="mw", Text="Mental Wellness"},
+            new SelectListItem {Value="ev", Text="Events"},
+            new SelectListItem {Value="sm", Text="Social Media"},
+            new SelectListItem {Value="oi", Text="Other Interest"},
+        };
+    }
+}
