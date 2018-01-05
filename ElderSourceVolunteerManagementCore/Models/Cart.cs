a5d7de2ee900d64b8d53efc2e4eaa1ce4840889a
s-waldron <30762163+s-waldron@users.noreply.github.com>
2018-01-05 16:00:24 -0500

    Finished filling out the cart class

--- ElderSourceVolunteerManagementCore/Models/Cart.cs
+++ ElderSourceVolunteerManagementCore/Models/Cart.cs
@@ -9,8 +9,24 @@ namespace ElderSourceVolunteerManagementCore.Models
 
         public virtual void AddItem(Opportunity opportunity)
         {
-
+            CartLine line = lineCollection
+                .Where(o => o.Opportunity.OPPORTUNITYID == opportunity.OPPORTUNITYID)
+                .FirstOrDefault();
+            if(line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Opportunity = opportunity
+                });
+            }// end if(line == null) check
         }// end AddItem method
+
+        public virtual void RemoveLine(Opportunity opportunity) =>
+            lineCollection.RemoveAll(l => l.Opportunity.OPPORTUNITYID == opportunity.OPPORTUNITYID);
+
+        public virtual void Clear() => lineCollection.Clear();
+
+        public virtual IEnumerable<CartLine> Lines => lineCollection;
 
         public class CartLine
         {
