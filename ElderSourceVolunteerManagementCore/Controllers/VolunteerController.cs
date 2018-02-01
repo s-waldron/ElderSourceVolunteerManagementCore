0ab0be27173d76bce14c45da3920b618946fa931
s-waldron <30762163+s-waldron@users.noreply.github.com>
2018-02-01 15:38:19 -0500

    Making futher changes towards finalizing the project.

--- ElderSourceVolunteerManagementCore/Controllers/VolunteerController.cs
+++ ElderSourceVolunteerManagementCore/Controllers/VolunteerController.cs
@@ -16,7 +16,7 @@ namespace ElderSourceVolunteerManagementCore.Controllers
             repository = repo;
         }// end VolunteerController constructor
 
-        [Authorize]
+        [Authorize(Roles = "Employee,Manager,Admin")]
         public ViewResult ListVolunteerEdit() => View(repository.Volunteer);
 
         // GET: /<controller>/
@@ -36,13 +36,13 @@ namespace ElderSourceVolunteerManagementCore.Controllers
             }// end else
         }// end CreateVolunteer method
 
-        [Authorize]
+        [Authorize(Roles = "Employee,Manager,Admin")]
         public ViewResult Create() => View("EmployeeForm", new Volunteer());
 
         public ViewResult Edit(int VolunteerID) => View("EmployeeForm",repository.Volunteer.FirstOrDefault
             (vol => vol.VOLUNTEERID == VolunteerID));
 
-        [Authorize]
+        [Authorize(Roles = "Employee,Manager,Admin")]
         [HttpPost]
         public IActionResult EmployeeForm(Volunteer volunteer)
         {
