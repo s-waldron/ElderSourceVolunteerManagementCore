6bec1203d95559f04939c2a3ea570107c593d467
Stephen Waldron <30762163+s-waldron@users.noreply.github.com>
2018-04-03 15:06:00 -0400

    updating report code

--- ElderSourceVolunteerManagementCore/Controllers/ReportController.cs
+++ ElderSourceVolunteerManagementCore/Controllers/ReportController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ElderSourceVolunteerManagementCore.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using OfficeOpenXml;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +15,70 @@ namespace ElderSourceVolunteerManagementCore.Controllers
 {
     public class ReportController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly IHostingEnvironment _hostingEnvironment;
+        
+        public ReportController(IHostingEnvironment hostingEnvironment)
         {
-            return View();
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public void Export()
+        {
+            string sWebRootFolder = _hostingEnvironment.WebRootPath;
+            string sFileName = @"report.xlsx";
+            FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+            if (file.Exists)
+            {
+                file.Delete();
+                file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+            }
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Report");
+                worksheet.Cells[1, 1].Value = "ID";
+                worksheet.Cells[1, 2].Value = "Name";
+                worksheet.Cells[1, 3].Value = "Gender";
+                worksheet.Cells[1, 4].Value = "Salary (in $)";
+
+                //Add values
+                worksheet.Cells["A2"].Value = 1000;
+                worksheet.Cells["B2"].Value = "Jon";
+                worksheet.Cells["C2"].Value = "M";
+                worksheet.Cells["D2"].Value = 5000;
+
+                worksheet.Cells["A3"].Value = 1001;
+                worksheet.Cells["B3"].Value = "Graham";
+                worksheet.Cells["C3"].Value = "M";
+                worksheet.Cells["D3"].Value = 10000;
+
+                worksheet.Cells["A4"].Value = 1002;
+                worksheet.Cells["B4"].Value = "Jenny";
+                worksheet.Cells["C4"].Value = "F";
+                worksheet.Cells["D4"].Value = 5000;
+
+                package.Save();
+            }
+        }
+        
+        // GET: /<controller>/
+        public IActionResult Index(string Report)
+        {
+            switch (Report)
+            {
+                case "aud":
+                    Export();
+                    downloadFile(_hostingEnvironment.WebRootPath, "report.xlsx");
+                    return View(new ReportViewModel());
+                case "v2o":
+                    return View(new ReportViewModel());                    
+                default:
+                    return View(new ReportViewModel());
+            }
+        }
+
+        public FileResult downloadFile(string filePath, string fileName)
+        {
+            return File("~\\wwwroot\\" + fileName, "text/csv", fileName);
         }
     }
 }
---
+++ ElderSourceVolunteerManagementCore/Models/ViewModels/ReportViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElderSourceVolunteerManagementCore.Models.ViewModels
+{
+    public class ReportViewModel
+    {
+        public string Report { get; set; }
+        public List<SelectListItem> Reports { get; } = new List<SelectListItem>
+        {
+            new SelectListItem {Value="aud", Text="Audit"},
+            new SelectListItem {Value="v2o", Text="Volunteer2Opportunity Hours Worked"},
+        };
+    }
+}
